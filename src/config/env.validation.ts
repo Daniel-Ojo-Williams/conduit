c94e300a82aa58b97e47855c8c0b4b00bcc2f834
Ojo-williams Daniel <freakydmuse@gmail.com>
2024-08-10 17:32:18 +0100

    chore: env variable validations
    This makes sure the application does not get started if required env variables are not supplied before starting

---
+++ src/config/env.validation.ts
@@ -0,0 +1,45 @@
+import { plainToInstance } from 'class-transformer';
+import { IsEnum, IsNumber, IsString, validateSync } from 'class-validator';
+
+enum Environment {
+  Development = 'development',
+  Production = 'production',
+}
+
+class EnvVariables {
+  @IsEnum(Environment)
+  NODE_ENV: Environment;
+
+  @IsNumber()
+  DB_PORT: number;
+
+  @IsString()
+  DB_HOST: string;
+
+  @IsString()
+  DB_USERNAME: string;
+
+  @IsString()
+  DB_PASSWORD: string;
+
+  @IsString()
+  DB_NAME: string;
+
+  @IsString()
+  DB_TYPE: string;
+}
+
+export function validateEnv(env: Record<string, unknown>) {
+  const validatedEnv = plainToInstance(EnvVariables, env, {
+    enableImplicitConversion: true,
+  });
+  const errors = validateSync(validatedEnv, {
+    skipMissingProperties: false,
+  });
+
+  if (errors.length > 0) {
+    throw new Error(errors.toString());
+  }
+
+  return validatedEnv;
+}
