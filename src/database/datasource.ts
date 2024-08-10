eb6fa4eff65983dc014ffa2b0a338118717d4b4b
Ojo-williams Daniel <freakydmuse@gmail.com>
2024-08-10 17:33:24 +0100

    chore: datasource for migration

---
+++ src/database/datasource.ts
@@ -0,0 +1,16 @@
+import { DataSource, DataSourceOptions } from 'typeorm';
+import 'dotenv/config';
+
+export const dataSourceOptions: DataSourceOptions = {
+  type: process.env.DB_TYPE as 'postgres',
+  host: process.env.DB_HOST,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  port: (process.env.DB_PORT as unknown as number) || 5432,
+  database: process.env.DB_NAME,
+  synchronize: false,
+  entities: ['dist/module/**/entities/*.js'],
+  migrations: ['dist/database/migrations/*.js'],
+};
+
+export const dataSource = new DataSource(dataSourceOptions);
