0a94559ad24acd3e79f54f983fa7e59444e81e2d
Ojo-williams Daniel <freakydmuse@gmail.com>
2024-08-11 22:42:48 +0100

    feat: add route prefix and validation pipe

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('/api', {
+    exclude: ['auth'],
+  });
+  app.useGlobalPipes(
+    new ValidationPipe({
+      forbidNonWhitelisted: true,
+    }),
+  );
   await app.listen(4000);
 }
 bootstrap();
